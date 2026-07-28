e465e5ce8de4747d1d82040c8aef3622281481db
MukunziRalf <152407477+MukunziRalf@users.noreply.github.com>
2026-07-28 10:53:02 +0000

    Use environment PORT and update launch config

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import apiApp from './src/api';
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   // Mount the API routes from our modular api module
   app.use(apiApp);
